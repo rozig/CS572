2549a5e073d34164152cc50eedc2709a70bcc1c2
Ganzorig Erdenebat <the.ganzorig25@gmail.com>
2018-04-12 14:02:44 -0500

    Midterm Programming question completed

--- midterm/programming/app.js
+++ midterm/programming/app.js
@@ -2,14 +2,11 @@ const express = require('express');
 const path = require('path');
 const cons = require('consolidate');
 const cookieParser = require('cookie-parser');
-const mongo = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
 
 const tasks = require('./routes/tasks');
 
 const app = express();
-
-const MongoServer = mongo.Server;
-const Db = mongo.Db;
 
 app.engine('html', cons.mustache);
 app.set('view engine', 'html');
@@ -20,12 +17,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const server = new MongoServer('localhost', 27017, {auto_reconnect: true});
-const db = new Db('todo_db', server);
-
-app.use((req, res, next) => {
-	req.db = db;
-	next();
+MongoClient.connect('mongodb://127.0.0.1:27017', (err, client) => {
+	app.db = client.db('test_db');
 });
 
 app.use('/tasks', tasks);
--- midterm/programming/routes/tasks.js
+++ midterm/programming/routes/tasks.js
@@ -1,22 +1,53 @@
 const express = require('express');
+const ObjectID = require('mongodb').ObjectID;
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	const collection = req.collection;
-
-	collection.find({completed: false}).toArray((err, tasks) => {
+	const db = req.app.db;
+	db.collection('tasks').find({status: 'incomplete'}, {$sort: {created_date: -1}}).toArray((err, tasks) => {
 		res.render('tasks', {tasks: tasks});
 	});
 });
 
 router.post('/', (req, res) => {
+	const task = {
+		task: req.body.task,
+		status: 'incomplete',
+		created_date: new Date(),
+		updated_date: ''
+	};
+	const db = req.app.db;
 
+	db.collection('tasks').insert(task, (err, taskInserted) => {
+		if(err) throw err;
+
+		res.redirect('/tasks');
+	});
 });
 
-router.get('/completed', (req, res) => {});
+router.get('/completed', (req, res) => {
+	const db = req.app.db;
+	db.collection('tasks').find({status: 'completed'}, {$sort: {created_date: -1}}).toArray((err, tasks) => {
+		res.render('tasks_completed', {tasks: tasks});
+	});
+});
 
-router.post('/:task_id', (req, res) => {});
+router.post('/:task_id', (req, res) => {
+	const id = new ObjectID(req.params.task_id);
+	const db = req.app.db;
 
-router.delete('/:task_id', (req, res) => {});
+	db.collection('tasks').update({_id: id}, {'$set': {status: 'completed', updated_date: new Date()}}, (err, numUpdated) => {
+		res.send({status: 'success'});
+	});
+});
+
+router.delete('/:task_id', (req, res) => {
+	const id = new ObjectID(req.params.task_id);
+	const db = req.app.db;
+
+	db.collection('tasks').remove({_id: id}, (err, result) => {
+		res.send({status: 'success'});
+	});
+});
 
 module.exports = router;
