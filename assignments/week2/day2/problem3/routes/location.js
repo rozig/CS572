1d6ac94edc26324e4d43fb026b186ff24ae0284c
Ganzorig Erdenebat <the.ganzorig25@gmail.com>
2018-04-11 22:04:12 -0500

    Changed tab indentation to spaces

--- assignments/week2/day2/problem3/routes/location.js
+++ assignments/week2/day2/problem3/routes/location.js
@@ -5,83 +5,83 @@ const router = express.Router();
 const dbUri = 'mongodb://127.0.0.1:27017';
 
 router.get('/new', (req, res) => {
-	res.render('add');
+    res.render('add');
 });
 
 router.get('/edit/:id', (req, res) => {
-	MongoClient.connect(dbUri, (err, client) => {
-		if(err) throw err;
-		const db = client.db('test_db');
-		const id = new mongo.ObjectID(req.params.id);
+    MongoClient.connect(dbUri, (err, client) => {
+        if(err) throw err;
+        const db = client.db('test_db');
+        const id = new mongo.ObjectID(req.params.id);
 
-		db.collection('locationPoints').findOne({_id: id}, (err, doc) => {
-			if(err) throw err;
-			console.dir(doc);
-			res.locals.location = doc;
-			res.render('edit');
-			return client.close();
-		});
-	});
+        db.collection('locationPoints').findOne({_id: id}, (err, doc) => {
+            if(err) throw err;
+            console.dir(doc);
+            res.locals.location = doc;
+            res.render('edit');
+            return client.close();
+        });
+    });
 });
 
 router.post('/new', (req, res) => {
-	MongoClient.connect(dbUri, (err, client) => {
-		if(err) throw err;
-		const db = client.db('test_db');
+    MongoClient.connect(dbUri, (err, client) => {
+        if(err) throw err;
+        const db = client.db('test_db');
 
-		const location = {
-			name: req.body.name,
-			category: req.body.category,
-			long: req.body.longitude,
-			lat: req.body.latitude
-		};
+        const location = {
+            name: req.body.name,
+            category: req.body.category,
+            long: req.body.longitude,
+            lat: req.body.latitude
+        };
 
-		db.collection('locationPoints').insert(location, (err, docInserted) => {
-			if(err) throw err;
+        db.collection('locationPoints').insert(location, (err, docInserted) => {
+            if(err) throw err;
 
-			res.redirect('/');
-			return client.close();
-		});
-	});
+            res.redirect('/');
+            return client.close();
+        });
+    });
 });
 
 router.post('/update/:id', (req, res) => {
-	MongoClient.connect(dbUri, (err, client) => {
-		if(err) throw err;
-		const db = client.db('test_db');
+    MongoClient.connect(dbUri, (err, client) => {
+        if(err) throw err;
+        const db = client.db('test_db');
 
-		const id = new mongo.ObjectID(req.params.id);
-		const location = {
-			name: req.body.name,
-			category: req.body.category,
-			long: req.body.longitude,
-			lat: req.body.latitude
-		};
+        const id = new mongo.ObjectID(req.params.id);
+        const location = {
+            name: req.body.name,
+            category: req.body.category,
+            long: req.body.longitude,
+            lat: req.body.latitude
+        };
 
-		db.collection('locationPoints').update({_id: id}, location, {upsert: true}, (err, numUpdated) => {
-			if(err) throw err;
+        db.collection('locationPoints').update({_id: id}, location, {upsert: true}, (err, numUpdated) => {
+            if(err) throw err;
 
-			res.redirect('/');
-			return client.close();
-		});
-	});
+            res.redirect('/');
+            return client.close();
+        });
+    });
 });
 
 router.get('/delete/:id', (req, res) => {
-	MongoClient.connect(dbUri, (err, client) => {
-		if(err) throw err;
-		const db = client.db('test_db');
+    MongoClient.connect(dbUri, (err, client) => {
+        if(err) throw err;
+        const db = client.db('test_db');
 
-		const id = new mongo.ObjectID(req.params.id);
+        const id = new mongo.ObjectID(req.params.id);
 
-		db.collection('locationPoints').remove({_id: id}, (err, result) => {
-			if(err) throw err;
+        db.collection('locationPoints').remove({_id: id}, (err, result) => {
+            if(err) throw err;
 
-			console.dir(result);
-			res.redirect('/');
-			return client.close();
-		});
-	});
+            console.dir(result);
+            res.redirect('/');
+            return client.close();
+        });
+    });
 });
 
 module.exports = router;
