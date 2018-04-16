ee290b7725b9ef054c0dd6f8cec1d11081f765c7
Ganzorig Erdenebat <the.ganzorig25@gmail.com>
2018-04-16 14:37:01 -0500

    Part of Assignment Week 2 Day 6

---
+++ assignments/week2/day6/types.ts
@@ -0,0 +1,26 @@
+interface BankAccount {
+	money: number;
+	deposit(value: number): void;
+}
+
+interface User {
+	name: string;
+	bankAccount: BankAccount;
+	hobbies: Array<string>;
+}
+
+let bankAccount: BankAccount = {
+	money: 2000,
+	deposit(value) {
+		this.money += value;
+	}
+};
+
+let myself: User = {
+	name: "Asaad",
+	bankAccount: bankAccount,
+	hobbies: ["Violin", "Cooking"]
+};
+
+myself.bankAccount.deposit(3000);
+console.log(myself);
